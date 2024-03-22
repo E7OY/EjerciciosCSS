c896e1a488430ea48b05f8a4f429a75442b3ef28
EloyGarcia <102689282+E7OY@users.noreply.github.com>
2024-03-22 12:05:11 +0100

    subida ejercicio final css

---
+++ EjercicioCSS/README.md
@@ -0,0 +1,11 @@
+# EjercicioCSS
+
+## Vista Previa
+
+![105shots_so](https://github.com/E7OY/EjercicioCSS/assets/102689282/ce125bbd-0411-4541-8338-c75ed4af8b1f)
+
+![374shots_so](https://github.com/E7OY/EjercicioCSS/assets/102689282/9ebb8f3a-a931-4637-aed3-95f65bcb0f97)
+
+![360shots_so](https://github.com/E7OY/EjercicioCSS/assets/102689282/4003f571-5c73-4e94-8022-968eb2326a67)
+
+![290shots_so](https://github.com/E7OY/EjercicioCSS/assets/102689282/e569713f-117d-4ef5-9427-7ab5f7ec741d)
---
+++ EjercicioCSS/main.js
@@ -0,0 +1,27 @@
+function flyToElement(flyer, flyingTo) {
+    var $func = $(this);
+    var divider = 3;
+    var flyerClone = $(flyer).clone();
+    $(flyerClone).css({position: 'absolute', top: $(flyer).offset().top + "px", left: $(flyer).offset().left + "px", opacity: 1, 'z-index': 1000});
+    $('body').append($(flyerClone));
+    var gotoX = $(flyingTo).offset().left + ($(flyingTo).width() / 2) - ($(flyer).width()/divider)/2;
+    var gotoY = $(flyingTo).offset().top + ($(flyingTo).height() / 2) - ($(flyer).height()/divider)/2;
+     
+    $(flyerClone).animate({
+        opacity: 0.5,
+        left: gotoX,
+        top: gotoY,
+        width: $(flyer).width()/divider,
+        height: $(flyer).height()/divider
+    }, 700,
+    function () {
+        $(flyingTo).fadeOut('fast', function () {
+            $(flyingTo).fadeIn('fast', function () {
+                $(flyerClone).fadeOut('fast', function () {
+                    $(flyerClone).remove();
+                });
+            });
+        });
+    });
+}
+
